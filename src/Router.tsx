ffa519dd2ea94fdf651953b9e131c5a7b24419a5
carlos <carlosramos10k@gmail.com>
2024-02-25 21:03:01 -0300

    config intial

---
+++ src/Router.tsx
@@ -0,0 +1,13 @@
+import {Routes, Route} from 'react-router-dom'
+import { Home } from './page/Home'
+import { DefaultLayout } from './Layouts/DefaultLayout'
+
+export function Router(){
+    return (
+        <Routes>
+            <Route path='/' element={<DefaultLayout/>}>
+                <Route path='/' element={<Home/>}/>
+            </Route>
+        </Routes>
+    )
+}
